f3a1372d82db5de5714a15fc674c7b74afc5f225
ErikaKoro <92887290+ErikaKoro@users.noreply.github.com>
2021-12-19 13:48:57 -0800

    comments

--- parallel/src/mpi_sort.c
+++ parallel/src/mpi_sort.c
@@ -3,9 +3,23 @@
 #include <mpi.h>
 #include "quick_select.h"
 
+
 /**
- * Finds the exchanges between the processes according to the following algorithm
- *
+ * This function finds the exchanges between the processes according to the following algorithm:
+ * counterReceiver::the array in which the master has gathered the number of points each process wants to exchange
+ * The main idea is to compare one by one the processes with ranks smaller than worldSize/2 to the processes with ranks bigger than worldSize/2,
+ * based on how many points each one wants to exchange. For example, let's consider the array with processes' ids
+ * and the counterReceiver array to be:
+ * ranks = [0 1 2 3 ]
+ * counterReceiver = [2 3 5 0]
+ * The aim is to give the points of the first half of the array to the second half. So, in the beginning we compare the counterReceiver[0] = 2 to
+ * counterReceiver[0 + worldSize/2] = counterReceiver[0 + 4/2] = counterReceiver[2] = 5.
+ * We see that 2 < 5 which means process with rank 0 should give to process with rank 2 2 points and vice versa.
+ * The process with rank 0 has completed its exchanges in contrast to the process with rank 2. The last one wants to give 5 - 2 = 3 more points to another process.
+ * So, now we have to compare the (i + 1)th element of the counterReceiver to the left points of the incomplete . We observe that counterReceiver[1] = 3
+ * is equal to the number of points that has remained in the process with rank 2. So, the process with rank 1 should give to process with rank 2
+ * 3 points and take from it 3 points as well.
+ * Then, both processes have completed their exchanges.This algorithm continues till all processes have exchanged as many elements as they wanted.
  *
  * @param counterReceiver The array with the number of points, each process wants to give, that has bees sent to the master
  * @param worldSize
@@ -24,47 +38,80 @@ int **findExchanges(int *counterReceiver, int worldSize){
 
     int nextIndex = worldSize / 2;  // Indicates the index of the second half of the array
     for(int i = 0; i < worldSize/2; i++){
-        while(counterReceiver[i] > 0){
-            if(counterReceiver[i] == counterReceiver[nextIndex]){
-                info[i][helperIndex[i]] = nextIndex;
+
+        while(counterReceiver[i] > 0){  // Repeat until each process gets the info for its exchanges. When counterReceiver[i] = 0 the rank doesn't want to exchange anymore or not at all
+
+            if(counterReceiver[i] == counterReceiver[nextIndex]){  // Compare elements of the first half of array to the second half's elements
+
+                info[i][helperIndex[i]] = nextIndex;  // Store for the rank i that it should give points to the nextIndex(it lies in the second half of the array as it is initialized with the value worldSize/2
+
+                helperIndex[i]++;  // Increase the index which indicates the last index we wrote sth about the i rank
+
+                info[i][helperIndex[i]] = counterReceiver[i];  // In the new index store how many points will the i rank send to the newIndex rank
+
                 helperIndex[i]++;
-                info[i][helperIndex[i]] = counterReceiver[i];
-                helperIndex[i]++;
-                info[nextIndex][helperIndex[nextIndex]] = i;
+
+                info[nextIndex][helperIndex[nextIndex]] = i;  // Store the info exchanges for the nextIndex rank as well
+                                                              // Send to the rank i
+
+                helperIndex[nextIndex]++;  // Increase the index which indicates the last index we wrote sth about the nextIndex rank
+
+                info[nextIndex][helperIndex[nextIndex]] = counterReceiver[i];  // Send to rank i as many elements as it sent to rank nextIndex
+
                 helperIndex[nextIndex]++;
-                info[nextIndex][helperIndex[nextIndex]] = counterReceiver[i];
-                helperIndex[nextIndex]++;
-                counterReceiver[i] = 0;
-                counterReceiver[nextIndex] = 0;
-                nextIndex++;
+
+                counterReceiver[i] = 0; // The i rank has gotten the info for its exchanges
+
+                counterReceiver[nextIndex] = 0;  // The nextIndex has gotten the info for its exchanges too
+
+                nextIndex++;  // Point to the new index we' re going to write
             }
-            else if(counterReceiver[i] < counterReceiver[nextIndex]){
+            else if(counterReceiver[i] < counterReceiver[nextIndex]){  // If the current rank wants to give less elements than the nextIndex requires
                 info[i][helperIndex[i]] = nextIndex;
+
                 helperIndex[i]++;
+
                 info[i][helperIndex[i]] = counterReceiver[i];
+
                 helperIndex[i]++;
+
                 info[nextIndex][helperIndex[nextIndex]] = i;
+
                 helperIndex[nextIndex]++;
+
                 info[nextIndex][helperIndex[nextIndex]] = counterReceiver[i];
+
                 helperIndex[nextIndex]++;
-                counterReceiver[nextIndex] -= counterReceiver[i];
-                counterReceiver[i] = 0;
+
+                counterReceiver[nextIndex] -= counterReceiver[i]; // the points remained that the nextIndex rank should exchange to finish its exchanges
+
+                counterReceiver[i] = 0; // The i rank has gotten all the info
             }
             else if(counterReceiver[i] > counterReceiver[nextIndex] && counterReceiver[nextIndex] != 0){
                 info[i][helperIndex[i]] = nextIndex;
+
                 helperIndex[i]++;
+
                 info[i][helperIndex[i]] = counterReceiver[nextIndex];
+
                 helperIndex[i]++;
+
                 info[nextIndex][helperIndex[nextIndex]] = i;
+
                 helperIndex[nextIndex]++;
+
                 info[nextIndex][helperIndex[nextIndex]] = counterReceiver[nextIndex];
+
                 helperIndex[nextIndex]++;
-                counterReceiver[i] -= counterReceiver[nextIndex];
-                counterReceiver[nextIndex] = 0;
+
+                counterReceiver[i] -= counterReceiver[nextIndex];  // The points left in i rank for which we should find with which next process should they be exchanged
+
+                counterReceiver[nextIndex] = 0;  // The nextIndex has gotten all its info
+
                 nextIndex++;
             }
             else{
-                nextIndex++;
+                nextIndex++;  // In case we have counterReceiver[i] > counterReceiver[nextIndex] && counterReceiver[nextIndex] == 0
             }
         }
     }
